0055d53610881914ab2ad7d72f9c7172ca45ef64
tommyrachmadiono <tommyrachmadiono@gmail.com>
2020-12-19 18:14:55 +0700

    gambar preview

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Material App',
+      title: 'Weather App',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.blue,
